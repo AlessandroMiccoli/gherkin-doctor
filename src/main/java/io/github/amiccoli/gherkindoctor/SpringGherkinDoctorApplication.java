b6df6b45426e8e799b0b6b42cc4a4152a326e094
Alessandro Miccoli <alessandro.miccoli@closedloopmedicine.com>
2025-02-02 14:17:18 +0000

    chore: init project

---
+++ src/main/java/io/github/amiccoli/gherkindoctor/SpringGherkinDoctorApplication.java
@@ -0,0 +1,12 @@
+package io.github.amiccoli.gherkindoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGherkinDoctorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGherkinDoctorApplication.class, args);
+    }
+}
+
